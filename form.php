0403e3ea3dae7f8b35942702d4ddacb6c5d424f6
Armen <91148484+heraxos@users.noreply.github.com>
2023-04-11 22:33:37 +0300

    Update form.php

--- form.php
+++ form.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style1.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	<title>Task-5</title>
+	<title></title>
 </head>
 <body>
 	<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm ">
